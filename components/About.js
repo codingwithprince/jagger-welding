df318448b271d353ae490adabe19d729a9b092f5
Prince Ahmed <codingwithprince@gmail.com>
2022-08-14 00:00:03 +0600

    added hero section

--- components/About.js
+++ components/About.js
@@ -9,8 +9,8 @@ const About = () => {
             <p className="about-text text-gray-600 text-justify text-md md:text-lg mb-5">
             The <span className='bg-yellow-300 text-black font-semibold px-1'>Jagger d.o.o.</span>  Company specialises in complex welding and installation services in industrial facilities and power plants. With our own highly qualified staff, we can carry out complex tasks in the fields of energy, industry and transport. Welders and pipe workers have knowledge on isometry and hold appropriate and valid certificates in accordance with international standards for various materials and welding techniques, which are periodically renewed.
             </p>
-            <div className="about-img">
-                <Image className='hover:scale-105 shadow-full ' src='/images/features/man_welding.png' height={400} width={400} alt='jagger-welding-man-welding'/>
+            <div className="about-img w-auto p-2">
+                <Image className='shadow-full' src='/images/features/man_welding.png' height={350} width={350} alt='jagger-welding-man-welding'/>
             </div>
         </div>
     </div>
--- components/Hero.js
+++ components/Hero.js
@@ -16,7 +16,7 @@ const Hero = () => {
         <div className="overlay h-full flex w-full justify-center md:justify-end items-center text-white px-[2%] md:px-20">
             <div className="text-overlay text-center md:text-left w-full md:w-[50%] p-2">
                 <h1 className='text-4xl inline-block p-2  w-full md:text-8xl animate-bounce uppercase russo-font gap-5'>Jagger</h1>
-                <div className='text-4xl  bg-gradient-to-b from-sky-500 to-sky-700 p-2 md:text-8xl uppercase russo-font'>
+                <div className='text-4xl rounded-sm  bg-gradient-to-b from-sky-500 to-sky-700 p-2 md:text-8xl uppercase russo-font'>
                 <TypeAnimation
                       sequence={['Welding  ', 4000, 'd.o.o', 4000]}
                       speed={40} // Must be in range between 1 and 99!
@@ -26,13 +26,13 @@ const Hero = () => {
                 </div>
                 <p className='mt-5 text-gray-100 md:text-left text-center font-semibold text-sm'>Meljska cesta 36 <br />2000 Maribor</p>
                 <div className="hero-btn mt-5 flex gap-3 justify-center md:justify-start font-bold ">
-                    <button className='shadow-lg hover:shadow-sky-600/50  md:w-auto text-xs md:text-md bg-gradient-to-b hover:bg-gradient-to-t from-sky-500 to-sky-700 text-white px-2 py-2'>
+                    <button className='shadow-lg rounded-full hover:shadow-sky-600/50  md:w-auto text-xs md:text-md bg-gradient-to-b hover:bg-gradient-to-t from-sky-500 to-sky-700 text-white px-2 py-2'>
                       <a className='flex justify-center items-center gap-1' href="tel: +386 41 637 807">
                       <IoCallSharp /> 
                       +386 41 637 807
                       </a>
                     </button>
-                    <button className='shadow-lg hover:shadow-sky-600/50  md:w-auto text-xs md:text-md bg-gradient-to-b hover:bg-gradient-to-t from-sky-500 to-sky-700  text-white px-5 py-2'>
+                    <button className='shadow-lg rounded-full hover:shadow-sky-600/50  md:w-auto text-xs md:text-md bg-gradient-to-b hover:bg-gradient-to-t from-sky-500 to-sky-700  text-white px-2 py-2'>
                       <a className='flex justify-center items-center gap-1' href="mailto:info@jagger-welding.com">
                       <IoMailSharp /> 
                       info@jagger-welding.com
