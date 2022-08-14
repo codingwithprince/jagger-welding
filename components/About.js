76cf3410945b142a6bb261e02cf825f22a4d9c32
Prince Ahmed <codingwithprince@gmail.com>
2022-08-14 20:04:51 +0600

    added about picture

--- components/About.js
+++ components/About.js
@@ -25,18 +25,18 @@ const About = () => {
   ]
   return (
     <div id="about" className="about py-[20px] px-[5%]">
-        <h3 className='font-bold text-center text-gray-700 text-2xl md:text-4xl mb-10'>About Jagger d.o.o.</h3>
+        <h3 className='font-bold text-center text-gray-700 text-2xl md:text-4xl my-3 md:my-10'>About Jagger d.o.o.</h3>
         <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center items-center py-5'>
             <p className="about-text text-gray-600 text-justify text-md md:text-lg mb-5 md:w-[60%]">
             The <span className='bg-yellow-300 text-black font-semibold px-1'>Jagger d.o.o.</span>  Company specialises in complex welding and installation services in industrial facilities and power plants. With our own highly qualified staff, we can carry out complex tasks in the fields of energy, industry and transport. Welders and pipe workers have knowledge on isometry and hold appropriate and valid certificates in accordance with international standards for various materials and welding techniques, which are periodically renewed.
             </p>
-            <div className="about-img w-auto mx-10 p-2">
+            <div className="about-img mx-10 p-2 w-[300px] md:w-auto">
               <Carousel autoPlay={true} infiniteLoop={true}>
                 {
                   images.map(item => {
                     return(
                       <div key={item.id}>
-                       <img className='shadow-full w-[220px] rounded-xl' src={item.src} alt='jagger-welding-man-welding'/>
+                       <img className='shadow-full rounded-xl' src={item.src} alt='jagger-welding-man-welding'/>
                         <p className="legend">Jagger d.o.o</p>
                       </div>
                     )
--- components/Services.js
+++ components/Services.js
@@ -1,5 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import { IoIosConstruct, IoIosCloudDone } from 'react-icons/io'
 
 const Services = () => {
     const services = [
@@ -42,16 +45,71 @@ const Services = () => {
         
 
     ]
+
+    const images =[
+          {
+            id: 1,
+            src: '/images/services/s-1.jpg'
+          }
+    ]
+
+    const serviceList = [
+        {
+            id:1,
+            title: 'manual electric-arc welding',
+            desc: 'Manual Metal Arc (MMA) welding is the most flexible and one of the most widely used arc welding processes'
+        },
+        {
+            id:2,
+            title: 'aluminium welding',
+            desc: ' GTAW is a great process for aluminum because it does not require mechanical wire feeding, which can create feedability issues.'
+        }
+    ]
   return (
-    <div id="services" className='services py-[20px] px-[5%]'>
-         <h3 className='font-bold text-center text-gray-700 text-2xl md:text-4xl mb-5'>Services</h3>
-         <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center py-5">
-            <div className="gif-welding p-2 hover:scale-105 animate duration-500 ease-in-out">
-                <img className='w-[400px]' src='/images/features/toon-welding.png' alt='' />
+    <div id="services" className='services py-[20px] pb-[50px] px-[5%] bg-slate-100'>
+         <h3 className='font-bold text-center text-gray-700 text-2xl md:text-4xl pb-10'>Services</h3>
+         <div className="service-cards flex flex-wrap justify-center">
+            {
+                serviceList.map(item => {
+                    return (
+                        <div key={item.id} className="s-card w-[300px] bg-white grid justify-items-center shadow-xl py-10 px-5 rounded-xl m-3 hover:shadow-2xl hover:scale-105 ease-in-out duration-300">
+                            <div className="card-header text-sky-500 text-6xl py-3">
+                                <IoIosConstruct />
+                            </div>
+                            <div className="s-card-text py-5">
+                                <h3 className="title text-gray-700 text-center font-semibold capitalize text-lg">
+                                    {item.title}
+                                </h3>
+                                <p className='px-2 py-5 text-justify text-gray-500 text-sm'>
+                                    {
+                                        item.desc
+                                    }
+                                </p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            
+         </div>
+         {/* <div className="grid grid-cols-1 md:grid-cols-2 justify-items-center items-center">
+            <div className="gif-welding mx-5 mb-3">
+                <Carousel autoPlay={true} showIndicators={false} showStatus={false} infiniteLoop={true} showThumbs={false}>
+                    {
+                    images.map(item => {
+                        return(
+                        <div key={item.id}>
+                        <img className='shadow-full w-[320px] rounded-lg' src={item.src} alt='jagger-welding-man-welding'/>
+                          
+                        </div>
+                        )
+                    })
+                    }
+                </Carousel>
             </div>
             <div className="services-text mx-auto">
                 <h5 className='text-gray-800 font-bold text-lg'>We carry out the following welding processes</h5>
-                <ul className='list-disc pl-5 mt-5 text-left py-5'>
+                <ul className='list-disc pl-5 mt-2 text-left py-3'>
                     {
                         services.map(item => {
                             return (
@@ -68,7 +126,7 @@ const Services = () => {
                 </ul>
             </div>
 
-         </div>
+         </div> */}
     </div>
   )
 }
