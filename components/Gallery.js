808e8b8c468fabcf23fe0207157859f158759d22
Prince Ahmed <codingwithprince@gmail.com>
2022-08-17 22:12:29 +0600

    added all photos

--- components/Gallery.js
+++ components/Gallery.js
@@ -41,14 +41,14 @@ const Gallery = () => {
     <div id='gallery' className='bg-slate-100 py-10'>
         <h3 data-aos="fade-up" className='font-bold text-center text-gray-700 text-2xl md:text-4xl pb-5'>Gallery</h3>
         <div data-aos='fade-up' className='py-5'>
-            <Marquee speed={15} pauseOnHover={true} gradient={false} direction='right'>
+            <Marquee speed={10} pauseOnHover={true} gradient={false} direction='right'>
                 {gallery.map((image , index) => (
                     <img onClick={ () => openImageViewer(index) } className='shadow-md h-[200px] md:h-[300px] max-w-none object-cover mx-2' key={image} src={image} alt="picsum" />
                 ))}
             </Marquee>
         </div>     
         <div data-aos='fade-up' className='py-5'>
-            <Marquee speed={10} pauseOnHover={true} gradient={false} direction='right'>
+            <Marquee speed={5} pauseOnHover={true} gradient={false} direction='right'>
                 {gallery.map((image , index) => (
                     <img onClick={ () => openImageViewer(index) } className='shadow-md h-[100px] md:h-[200px] max-w-none object-cover mx-2' key={image} src={image} alt="picsum" />
                 )).reverse()}
